cbbaa3550806808e226a4829187d3ad8439e2634
Alex Su <alexanderytsu@gmail.com>
2020-12-04 15:08:59 +1100

    Updates

---
+++ precache-manifest.1a534995b6fbcaf5933bddb1b1ce1300.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252b459e7731c7046bec98b9be6f87a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85cf2b07d658b4d219d9",
+    "url": "/static/js/2.8d1067dc.chunk.js"
+  },
+  {
+    "revision": "1b542a2a6ea4505a36f2e021fedcfcb3",
+    "url": "/static/js/2.8d1067dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1bed7a4e7083890046ee",
+    "url": "/static/js/main.a0d72102.chunk.js"
+  },
+  {
+    "revision": "5735ed579fdfe8d0fdca",
+    "url": "/static/js/runtime-main.346f6d05.js"
+  }
+]);
